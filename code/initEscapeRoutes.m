66b87697c008a2270748dd57b80f6afa9ce15f49
Felix Thaler <felix@nummi.ch>
2012-05-19 20:26:37 +0200

    changed excape route calculation, so agents use exits on higher floors, too. This is just a hack, we still do not consider the additional path length when using a stair instead of an exit on the current floor...

--- code/initEscapeRoutes.m
+++ code/initEscapeRoutes.m
@@ -6,11 +6,11 @@ for i=1:data.floor_count
 
     boundary_data = zeros(size(data.floor(i).img_wall));
     boundary_data(data.floor(i).img_wall) =  1;
-    if (i == 1)
+    %if (i == 1)
         boundary_data(data.floor(i).img_exit) = -1;
-    else
+    %else
         boundary_data(data.floor(i).img_stairs_down) = -1;
-    end
+    %end
     
     exit_dist = fastSweeping(boundary_data) * data.meter_per_pixel;
     [data.floor(i).img_dir_x, data.floor(i).img_dir_y] = ...
